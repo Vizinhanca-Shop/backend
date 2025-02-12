fa56afc36d5e75ce7def7317e9205f96218c7684
Carlos Eduardo <eduardorosa123321@gmail.com>
2025-02-12 15:24:52 -0300

    refactor: add 'jwk' to publicRoutes in auth middleware for improved route handling

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -16,7 +16,7 @@ interface AuthMiddlewareRequest extends Request {
   }
 }
 
-const publicRoutes = ['auth', 'public', 'state', 'schema']
+const publicRoutes = ['auth', 'public', 'state', 'schema', 'jwk']
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   async use(req: AuthMiddlewareRequest, next: (value?: unknown) => void) {
