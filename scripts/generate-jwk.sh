8b70d663dc27bd6cef529f16a46190bf9708265d
Leonardo Souza <leonardo.souza@plathanus.tech>
2025-02-18 13:17:02 -0300

    refactor: change deployment flow

--- scripts/generate-jwk.sh
+++ scripts/generate-jwk.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-mkdir -p /usr/src/app/public/jwk
+if [ -z "$JWT_SECRET" ]; then
+  echo "Please set the JWT_SECRET environment variable"
+  exit 1
+fi
 
-K_VALUE=$(echo -n "$JWT_REFRESH_SECRET" | base64)
+mkdir -p ./public/jwk
+K_VALUE=$(echo -n "$JWT_SECRET" | base64)
 
-cat > /usr/src/app/public/jwk/symmetric.json << EOF
+cat > ./public/jwk/symmetric.json << EOF
 {
   "keys": [
     {
