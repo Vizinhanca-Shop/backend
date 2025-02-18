2ef537515d2dec6e51edfae96ae8d2172bffda41
Leonardo Souza <leonardo.souza@plathanus.tech>
2025-02-18 15:35:03 -0300

    set hardcode

--- scripts/generate-jwk.sh
+++ scripts/generate-jwk.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -z "$JWT_REFRESH_SECRET" ]; then
-  echo "Please set the JWT_REFRESH_SECRET environment variable"
-  exit 1
-fi
+# if [ -z "$JWT_REFRESH_SECRET" ]; then
+#   echo "Please set the JWT_REFRESH_SECRET environment variable"
+#   exit 1
+# fi
 
 mkdir -p ./public/jwk
-K_VALUE=$(echo -n "$JWT_REFRESH_SECRET" | base64)
+K_VALUE=$(echo -n "EyGqyoW9imynr5d4çaK3f9" | base64)
 
 cat > ./public/jwk/symmetric.json << EOF
 {
